23fa56cdb2d24ac9ca85383323b2da497afd4115
valentino <valenkvolek@gmail.com>
2025-04-04 15:24:02 -0300

    ejer4prac2

--- AYED/src/main/java/tp1/ejercicio8/CircularQueue.java
+++ AYED/src/main/java/tp1/ejercicio8/CircularQueue.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class CircularQueue<T> extends Queue<T> {
 
     public T shift() {
-        T primerElem = dequeue();  //saco el primer elemento
+        T primerElem = dequeue(); //saco el primer elemento
         enqueue(primerElem); // agrego  al final
         return primerElem;  // retorno el primer elemento
     }
--- AYED/src/main/java/tp1/ejercicio8/DoubleEndedQueue.java
+++ AYED/src/main/java/tp1/ejercicio8/DoubleEndedQueue.java
@@ -1,8 +1,9 @@
-package tp1.ejercicio8;
+package main.java.tp1.ejercicio8;
 
 import java.util.LinkedList;
 
 public class DoubleEndedQueue<T> {
+
     private LinkedList<T> dato;
 
     public T dequeueFirst() {
