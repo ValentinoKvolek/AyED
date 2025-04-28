c89c8cd46140943c650de389f28ab9410a68e666
ValentinoKvolek <valenkvolek@gmail.com>
2025-04-28 17:49:24 -0300

    ordenando

--- AYED/src/main/java/tp1/ejercicio8/Sequence.java
+++
@@ -1,6 +0,0 @@
-package main.java.tp1.ejercicio8;
-
-public abstract class Sequence {
-    int size(){return 0;};
-    boolean isEmpty(){return true;};
-}
--- AYED/src/main/java/tp1/ejercicio9/Sequence.java
+++
@@ -1,6 +0,0 @@
-package main.java.tp1.ejercicio9;
-
-public abstract class Sequence {
-    public abstract int size();
-    public abstract boolean isEmpty();
-}
