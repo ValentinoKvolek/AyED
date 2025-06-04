cf27b335448386cc2665eb21cf4dac2d1f17bb50
ValentinoKvolek <valenkvolek@gmail.com>
2025-06-04 09:50:48 -0300

    prac5

--- Resoluciones/AYED/src/main/java/tp5/ejercicio2/Recorridos.java
+++ Resoluciones/AYED/src/main/java/tp5/ejercicio2/Recorridos.java
@@ -1,11 +1,13 @@
 package main.java.tp5.ejercicio2;
 
+import main.java.tp1.ejercicio8.Queue;
 import main.java.tp5.ejercicio1.Edge;
 import main.java.tp5.ejercicio1.Graph;
 import main.java.tp5.ejercicio1.Vertex;
 
 import java.util.LinkedList;
 import java.util.List;
+
 
 public class Recorridos <T> {
 
@@ -48,6 +50,45 @@ public class Recorridos <T> {
             }
         }
 
+    }
+
+    //bfs(Graph<T> grafo): List<T>
+    //Retorna una lista con los datos de vértices, con el recorrido en amplitud del grafo recibido como parámetro.
+
+    public List<T> bfs (Graph<T> grafo){
+
+        List<T> res = new LinkedList<>();
+        if (grafo == null || grafo.getSize() == 0) {
+            return res;
+        }
+        boolean[] marca = new boolean[grafo.getSize()];
+        for (int i = 0; i < grafo.getSize(); i++){
+            if (!marca[i]){
+                this.bfs(i,grafo,marca,res);
+            }
+        }
+
+        return res;
+
+    }
+
+    private void bfs (int i, Graph<T> grafo, boolean[] marca, List<T> res){\
+
+        Queue<Vertex<T>> q = new Queue<Vertex<T>>(); //cola de vertices
+        q.enqueue(grafo.getVertex(i));
+        marca[i] = true;
+
+        //en colo el vertice en el cola y lo marco como visitado.
+        while (!q.isEmpty()){
+            Vertex<T> w = q.dequeue();
+            res.add(w.getData());
+            List<Edge<T>> adyacentes = grafo.getEdges(w);
+            for (Edge<T> e : adyacentes){
+                int j = e.getTarget().getPosition();
+                marca[j] = true;
+                q.enqueue(e.getTarget());
+            }
+        }
     }
 
 }
