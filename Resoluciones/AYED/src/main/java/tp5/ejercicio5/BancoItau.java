5a7dcf42dfd779cc3b3ca6f1f20e7358a583020d
ValentinoKvolek <valenkvolek@gmail.com>
2025-06-07 11:19:59 -0300

    prac5

--- Resoluciones/AYED/src/main/java/tp5/ejercicio5/BancoItau.java
+++ Resoluciones/AYED/src/main/java/tp5/ejercicio5/BancoItau.java
@@ -4,6 +4,7 @@ import main.java.tp1.ejercicio8.Queue;
 import main.java.tp5.ejercicio1.Edge;
 import main.java.tp5.ejercicio1.Graph;
 import main.java.tp5.ejercicio1.Vertex;
+import main.java.tp5.ejercicio1.adjList.AdjListGraph;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -74,6 +75,44 @@ public class BancoItau {
                     return;
                 }
             }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Graph<Persona> grafo = new AdjListGraph<>();
+
+
+        Vertex<Persona> v1 = grafo.createVertex(new Persona(false, "Matias", "AAA")); // empleado
+        Vertex<Persona> v2 = grafo.createVertex(new Persona(true, "Julian", "BBB"));  // jubilado
+        Vertex<Persona> v3 = grafo.createVertex(new Persona(true, "Francisco", "CCC")); // jubilado
+        Vertex<Persona> v4 = grafo.createVertex(new Persona(false, "Valentin", "DDD")); // empleado
+        Vertex<Persona> v5 = grafo.createVertex(new Persona(true, "Omar", "EEE"));     // jubilado
+        Vertex<Persona> v6 = grafo.createVertex(new Persona(false, "Rosana", "FFF"));  // empleado
+        Vertex<Persona> v7 = grafo.createVertex(new Persona(true, "Maria", "GGG"));    // jubilado
+        Vertex<Persona> v8 = grafo.createVertex(new Persona(true, "Sandra", "HHH"));   // jubilado
+        Vertex<Persona> v9 = grafo.createVertex(new Persona(true, "Matheo", "III"));   // jubilado
+
+        // conexiones bidireccionales
+        grafo.connect(v1, v2); grafo.connect(v2, v1);
+        grafo.connect(v2, v3); grafo.connect(v3, v2);
+        grafo.connect(v1, v9); grafo.connect(v9, v1);
+        grafo.connect(v9, v8); grafo.connect(v8, v9);
+        grafo.connect(v1, v4); grafo.connect(v4, v1);
+        grafo.connect(v1, v6); grafo.connect(v6, v1);
+        grafo.connect(v4, v5); grafo.connect(v5, v4);
+        grafo.connect(v5, v7); grafo.connect(v7, v5);
+
+        BancoItau banco = new BancoItau();
+
+        Persona empleado = v1.getData(); // Matias
+        int gradoSeparacion = 3;
+
+        List<Persona> resultado = banco.listaJubilados(grafo, empleado, gradoSeparacion);
+
+        System.out.println("Jubilados alcanzados por Matias (grado ≤ " + gradoSeparacion + "):");
+        for (Persona p : resultado) {
+            System.out.println("- " + p.getNombre() + " (" + p.getDomicilio() + ")");
         }
     }
 }
